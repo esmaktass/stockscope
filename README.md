c34f67c3a10b50c8104678343fc4508d3c9b306d
Esma Nur Aktaş <esmanuraktas11@gmail.com>
2026-08-02 15:11:39 +0300

    chore: initialize StockScope project

---
+++ README.md
@@ -0,0 +1,24 @@
+# StockScope
+
+StockScope is a stock market dashboard built with Python.
+
+## Features
+
+- Fetch stock data using Yahoo Finance API
+- Display latest closing price
+- Calculate daily price change
+- Calculate daily percentage change
+
+## Technologies
+
+- Python
+- Pandas
+- yfinance
+
+## Planned Features
+
+- Interactive charts with Plotly
+- Streamlit dashboard
+- Moving Averages (MA20 / MA50)
+- RSI Indicator
+- Multiple stock comparison
--- data.py
+++ data.py
@@ -1,16 +1,30 @@
 import yfinance as yf
 
-stock = yf.Ticker("AAPL")
-data = stock.history(period="1y")
+while True:
+    hisse = input("Hisse kodunu giriniz: ")
+    ticker = hisse.strip().upper()
+    stock = yf.Ticker(ticker)
+    data = stock.history(period="6mo")
 
-print("\nSon 5 satır:")
-print(data.tail())
+    if data.empty :
+        print("Bu hisse kodu bulunamadı. Lütfen kodu kontrol edip tekrar deneyin.")
+        continue
 
-print("\nSütunlar:")
-print(data.columns)
+    close_prices = data["Close"].dropna()
 
-print("\nVeri boyutu:")
-print(data.shape)
+    if len(close_prices) < 2:
+        print("Yeterli veri bulunamadı. Lütfen başka bir hisse kodu deneyin.")
+        continue
 
-print("\nVeri tipi:")
-print(type(data))
+    else :
+        current_price = close_prices.iloc[-1]
+        previous_price = close_prices.iloc[-2]
+        daily_change = current_price - previous_price
+        daily_change_percent = (daily_change/previous_price)*100
+        print(f"Hisse kodu: {ticker}")
+        print(f"Son kapanış fiyatı: {current_price:.2f}")
+        print(f"Önceki kapanış fiyatı: {previous_price:.2f}")
+        print(f"Günlük fiyat değişimi: {daily_change:.2f}")
+        print(f"Günlük değişim: {daily_change_percent:.2f}%")
+        break
+ 
